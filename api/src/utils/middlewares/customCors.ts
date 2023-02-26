7741b09b3ae3478d6f77f6d566f436c06bcd6e73
Rasul Sofiyev <sofiyevsr@gmail.com>
2023-02-26 15:38:14 +0400

    fix cors

--- api/src/utils/middlewares/customCors.ts
+++ api/src/utils/middlewares/customCors.ts
@@ -5,31 +5,30 @@ export const cors: MiddlewareHandler<string, CustomEnvironment> = async (
 	c,
 	next
 ) => {
-  const res = c.res.clone();
 	const options = {
 		origin: c.env.ORIGIN,
 		allowMethods: ["GET", "HEAD", "PUT", "POST", "DELETE", "PATCH"],
 		allowHeaders: ["Content-Type"],
 	};
-	res.headers.append("Access-Control-Allow-Origin", options["origin"]);
-	res.headers.append("Access-Control-Allow-Credentials", "true");
-	res.headers.append(
+	c.res.headers.append("Access-Control-Allow-Origin", options["origin"]);
+	c.res.headers.append("Access-Control-Allow-Credentials", "true");
+	c.res.headers.append(
 		"Access-Control-Allow-Headers",
 		options["allowHeaders"].join(",")
 	);
-	res.headers.append("Vary", "Origin");
+	c.res.headers.append("Vary", "Origin");
 
 	if (c.req.method !== "OPTIONS") {
 		return await next();
 	}
 	// Preflight
-	res.headers.append(
+	c.res.headers.append(
 		"Access-Control-Allow-Methods",
 		options.allowMethods.join(",")
 	);
 
-	res.headers.delete("Content-Length");
-	res.headers.delete("Content-Type");
+	c.res.headers.delete("Content-Length");
+	c.res.headers.delete("Content-Type");
 
 	c.res = new Response(null, {
 		headers: c.res.headers,
