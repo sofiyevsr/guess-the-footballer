cb0be3d688e2f239c4de3fd19638cef770726417
Rasul Sofiyev <sofiyevsr@gmail.com>
2023-03-29 02:15:37 +0400

    fix

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: { runtime: "edge" },
+  experimental: { runtime: "experimental-edge" },
   images: {
     remotePatterns: [
       {
