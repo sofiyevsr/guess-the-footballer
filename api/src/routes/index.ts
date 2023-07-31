50cc0f5e0a4c5816b27a7dee2fea25586254ae59
Rasul Sofiyev <sofiyevsr@gmail.com>
2023-07-31 23:29:25 +0400

    delete old api

--- api/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import multiplayerRouter from "./multiplayer";
-import playerRouter from "./player";
-import sessionRouter from "./session";
-
-const routes = { sessionRouter, multiplayerRouter, playerRouter };
-
-export default routes;
