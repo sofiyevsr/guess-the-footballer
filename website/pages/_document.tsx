dcecdc869c59696657d4b823fc6bea2ca4da57e1
Rasul Sofiyev <sofiyevsr@gmail.com>
2023-08-06 14:36:45 +0400

    fix head issue in _document

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
+      <Head />
       <body>
-        <Head />
         <Main />
         <NextScript />
       </body>
