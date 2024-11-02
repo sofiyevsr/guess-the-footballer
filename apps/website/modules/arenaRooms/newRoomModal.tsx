fa2288bd37124d709e19e38b078aa19da27b2908
Rasul Sofiyev <sofiyevsr@gmail.com>
2024-11-02 23:41:47 +0400

    fix: fix modals

--- apps/website/modules/arenaRooms/newRoomModal.tsx
+++ apps/website/modules/arenaRooms/newRoomModal.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import clsx from "classnames";
 import { SingleRoom } from "utils/services/arena/types";
 import Clipboard from "@cmpt/misc/clipboard";
 import { useRouter } from "next/router";
 import { WEBSITE_URL } from "utils/constants";
+import {
+  DialogPanel,
+  DialogTitle,
+  Dialog,
+  DialogBackdrop,
+} from "@headlessui/react";
 
 interface Props {
   room: SingleRoom | undefined;
@@ -13,40 +18,43 @@ interface Props {
 export const NewRoomModal = ({ room, reset }: Props) => {
   const { push } = useRouter();
   return (
-    <div
-      className={clsx("modal modal-bottom sm:modal-middle", {
-        "modal-open": room != null,
-      })}
+    <Dialog
+      open={room != null}
+      onClose={() => reset()}
+      className="relative z-50"
     >
-      <div className="modal-box">
-        <h3 className="font-bold text-lg">
-          You have created a new room successfully
-        </h3>
-        <p className="py-4">Do you want to join the room now?</p>
-        <Clipboard
-          text={`${WEBSITE_URL}/arena/${room?.id ?? ""}`}
-          label="Click to copy room's url"
-        />
-        <div className="modal-action">
-          <button
-            className="btn btn-error text-white"
-            onClick={() => {
-              reset();
-            }}
-          >
-            Close
-          </button>
-          <button
-            className="btn btn-success text-white"
-            onClick={() => {
-              if (room == null) return;
-              push({ pathname: "/arena/[id]", query: { id: room.id } });
-            }}
-          >
-            Join
-          </button>
-        </div>
+      <DialogBackdrop className="fixed inset-0 bg-black/30" />
+      <div className="fixed inset-0 flex w-screen items-center justify-center">
+        <DialogPanel className="mx-8 w-[32rem] bg-base-200 p-8 rounded-xl">
+          <DialogTitle className="font-bold">
+            You have created a new room successfully
+          </DialogTitle>
+          <p className="py-4">Do you want to join the room now?</p>
+          <Clipboard
+            text={`${WEBSITE_URL}/arena/${room?.id ?? ""}`}
+            label="Click to copy room's url"
+          />
+          <div className="flex gap-2 justify-end mt-4">
+            <button
+              className="btn btn-error text-white"
+              onClick={() => {
+                reset();
+              }}
+            >
+              Close
+            </button>
+            <button
+              className="btn btn-success text-white"
+              onClick={() => {
+                if (room == null) return;
+                push({ pathname: "/arena/[id]", query: { id: room.id } });
+              }}
+            >
+              Join
+            </button>
+          </div>
+        </DialogPanel>
       </div>
-    </div>
+    </Dialog>
   );
 };
--- apps/website/modules/play/playSettingsModal.tsx
+++ apps/website/modules/play/playSettingsModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import clsx from "classnames";
 import { useRouter } from "next/router";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 
 interface Props {
   listID: string | undefined;
@@ -23,50 +24,52 @@ export const PlaySettingsModal = ({ listID, setListID }: Props) => {
   }
 
   return (
-    <dialog
-      className={clsx("modal max-lg:modal-bottom", {
-        "modal-open": listID != null,
-      })}
+    <Dialog
+      open={listID != null}
+      onClose={() => onClose()}
+      className="relative z-50"
     >
-      <div className="modal-backdrop" onClick={onClose} />
-      <div className="modal-box flex flex-col">
-        <label className="font-bold text-lg">Pick the number of rounds</label>
-        <select
-          className={clsx("mt-4 select select-bordered w-full max-w-xs", {
-            "select-error": roundsError,
-          })}
-          onChange={(e) => {
-            setRoundsError(false);
-            setRounds(e.currentTarget.value);
-          }}
-          defaultValue="default"
-        >
-          <option disabled value="default">
-            Number of rounds
-          </option>
-          {options.map((option) => (
-            <option key={option.value}>{option.label}</option>
-          ))}
-        </select>
-        <div className="modal-action">
-          <button className="btn btn-error text-white" onClick={onClose}>
-            Close
-          </button>
-          <button
-            className="btn btn-success text-white"
-            onClick={() => {
-              if (listID == null) return;
-              if (rounds == null) return setRoundsError(true);
-              push({
-                pathname: "/play/view",
-                search: `listID=${listID}&rounds=${rounds}`,
-              });
+      <DialogBackdrop className="fixed inset-0 bg-black/30" />
+      <div className="fixed inset-0 flex w-screen items-center justify-center">
+        <DialogPanel className="mx-8 w-[32rem] bg-base-200 p-8 rounded-xl">
+          <label className="font-bold text-lg">Pick the number of rounds</label>
+          <select
+            className={clsx("mt-4 select select-bordered w-full max-w-xs", {
+              "select-error": roundsError,
+            })}
+            onChange={(e) => {
+              setRoundsError(false);
+              setRounds(e.currentTarget.value);
             }}
+            defaultValue="default"
           >
-            START
-          </button>
-        </div>
+            <option disabled value="default">
+              Number of rounds
+            </option>
+            {options.map((option) => (
+              <option key={option.value}>{option.label}</option>
+            ))}
+          </select>
+          <div className="modal-action">
+            <button className="btn btn-error text-white" onClick={onClose}>
+              Close
+            </button>
+            <button
+              className="btn btn-success text-white"
+              onClick={() => {
+                if (listID == null) return;
+                if (rounds == null) return setRoundsError(true);
+                push({
+                  pathname: "/play/view",
+                  search: `listID=${listID}&rounds=${rounds}`,
+                });
+              }}
+            >
+              START
+            </button>
+          </div>
+        </DialogPanel>
       </div>
-    </dialog>
+    </Dialog>
   );
 };
