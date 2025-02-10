9528a86e21eb4a05891513292b41298691e2f881
Rasul Sofiyev <sofiyevsr@gmail.com>
2025-02-10 20:52:28 +0400

    fix: fix date issue with challenges

--- apps/api/src/utils/misc/date.ts
+++ apps/api/src/utils/misc/date.ts
@@ -4,7 +4,7 @@ interface MonthRangeInput {
 }
 
 export function getMonthRange({ year, month }: MonthRangeInput) {
-	const startOfMonth = new Date(year, month, 1);
-	const endOfMonth = new Date(year, month + 1, 1);
+	const startOfMonth = new Date(Date.UTC(year, month, 1));
+	const endOfMonth = new Date(Date.UTC(year, month + 1, 1));
 	return [startOfMonth, endOfMonth] as const;
 }
